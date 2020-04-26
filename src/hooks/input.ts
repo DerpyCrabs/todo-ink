b6ed3325b259a5dd0a796596dc559baf5e6bf4b9
DerpyCrabs <derpycrabs@gmail.com>
2020-04-26 19:16:54 +0700

    Rewrite useInput using useInput from new version of ink

---
+++ src/hooks/input.ts
@@ -0,0 +1,24 @@
+import { Key as KeyWithoutBackspace, useInput } from 'ink'
+
+export interface Key extends KeyWithoutBackspace {
+  backspace: boolean
+}
+
+type InputHandler = (input: string, key: KeyWithoutBackspace) => void
+
+export type Handler = (input: string, key: Key) => void
+
+const handleBackspace = (handler: InputHandler) => (
+  input: string,
+  key: Key
+) => {
+  if (input === '\x08' || input === '\x7F') {
+    return handler(input, { ...key, backspace: true })
+  } else {
+    return handler(input, { ...key, backspace: false })
+  }
+}
+const useInputFix = (handler: Handler, options = { active: true }) =>
+  useInput(handleBackspace(handler), { isActive: options.active })
+
+export default useInputFix
